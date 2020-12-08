d13a0b17b6365d0445a6f25e5d75583721a42c4d
friday27 <yiyinghahalin@gmail.com>
2020-12-08 23:38:21 +0000

    rewrite for data format

--- app.js
+++ app.js
@@ -3,77 +3,108 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const URL = "http://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL?response=open_dat"
+const URL =
+  "http://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL?response=open_dat";
 
 async function getDailyData() {
   try {
     const res = await axios.get(URL);
     const date = res.data.date;
-    const d = ["./data", date.slice(0,4), date.slice(4,6), date.slice(6)];
 
-    for (let i = 0; i < 3; i++) {
-      const dir = d.slice(0, i+1).join("/");
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-        console.log(`dir ${dir} created`);
-      }
+    const stocks = {};
+    res.data.data.forEach((stockArr) => {
+      stocks[stockArr[0]] = {
+        avgPrice: (parseFloat(stockArr[4]) + parseFloat(stockArr[7])) / 2,
+        totalAmount: stockArr[2],
+      };
+    });
+
+    // concat data
+    let pastData = {};
+    const filename = `./data/${date.slice(0, 6)}-stock-prices.json`;
+    try {
+      pastData = JSON.parse(fs.readFileSync(filename));
+    } catch (e) {
+      // skip if the data doesn't exist
     }
+    pastData[date] = stocks;
 
-    fs.writeFileSync(`./data/${d[1]}/${d[2]}/${date}.json`, JSON.stringify(res.data, null, 4));
+    fs.writeFileSync(filename, JSON.stringify(pastData, null, 2));
+
     console.log(`${date} data saved`);
   } catch (e) {
     console.log(`getDailyData: ${e}`);
   }
 }
 
-function calculateAvg(days=7) {
+function calculateAvg(days = 7) {
   let dates = [];
   const today = new Date();
   const time = today.getTime();
-  for (let i = days; i >= 0 ; i--) {
-    const tmpDate = new Date(time - (24*60*60*1000*i)); 
-    dates.push(`${tmpDate.getFullYear()}${tmpDate.getMonth()+1}${("0"+tmpDate.getDate()).slice(-2)}`);
+  for (let i = days; i >= 0; i--) {
+    const tmpDate = new Date(time - 24 * 60 * 60 * 1000 * i);
+    dates.push(
+      `${tmpDate.getFullYear()}${tmpDate.getMonth() + 1}${(
+        "0" + tmpDate.getDate()
+      ).slice(-2)}`
+    );
   }
 
   const stocks = {};
-  
   for (let date of dates) {
     try {
-      const res = fs.readFileSync(`./data/${date.slice(0,4)}/${date.slice(4,6)}/${date}.json`);
-      const data = JSON.parse(res).data;
-      for (let row of data) {
-        const [stockId, , , , open, , , close, , ] = row;
-        if (!stocks[stockId]) stocks[stockId] = [];
-        stocks[stockId].push((parseFloat(open)+parseFloat(close))/2);
+      let monthlyData = JSON.parse(
+        fs.readFileSync(`./data/${date.slice(0, 6)}-stock-prices.json`)
+      );
+      const data = monthlyData[date];
+      const stockIds = Object.keys(data);
+      for (const stockId of stockIds) {
+        if (!stocks[stockId]) stocks[stockId] = { prices: [], amounts: [] };
+        stocks[stockId].prices.push(data[stockId].avgPrice);
+        stocks[stockId].amounts.push(data[stockId].totalAmount);
       }
-    } catch (e) { // skip if the data doesn't exist
-      continue;
+    } catch (e) {
+      // skip if the data doesn't exist
     }
   }
 
-  const targets = [];
   for (let stockId of Object.keys(stocks)) {
-    let prices = stocks[stockId];
-    const lastPrice = prices[prices.length-1];
-    prices = prices.slice(0, prices.length-1);
-    const avg = prices.reduce((a, b) => a+b, 0) / prices.length;
+    let prices = stocks[stockId].prices;
+    const lastPrice = prices[prices.length - 1];
+    prices = prices.slice(0, prices.length - 1);
+    const avg = prices.reduce((a, b) => a + b, 0) / prices.length;
 
     // calculate deviation
     let dev = 0;
-    for (let p of prices) dev += (p-avg)**2;
-    dev = (dev/prices.length)**0.5;
+    for (let p of prices) dev += (p - avg) ** 2;
+    dev = (dev / prices.length) ** 0.5;
 
-    if (lastPrice < avg-2*dev) {
-      targets.push(stockId);
-      console.log(`${stockId} price: ${Math.round(lastPrice, 2)} avg: ${Math.round(avg, 2)} threshold: ${Math.round(avg-2*dev, 2)}`);
-    }
+    stocks[stockId].avgPrice = avg;
+    stocks[stockId].targetPrice = avg - dev * 2;
+
+    let avgAmount = 0;
+    stocks[stockId].amounts.forEach(a => avgAmount += parseInt(a.replace(",", "")));
+    stocks[stockId].avgAmount = avgAmount / stocks[stockId].amounts.length;
   }
-  console.log(`Targets: ${targets}`);
+
+  // sort stocks by avg amounts and the gap between target and avg prices
+  const sortedStocks = Object.keys(stocks).sort(
+    (a, b) =>
+      stocks[b].avgAmount -
+      stocks[a].avgAmount +
+      (stocks[b].avgPrice -
+        stocks[b].targetPrice -
+        (stocks[a].avgPrice - stocks[a].targetPrice))
+  );
+
+  for (let i = 0; i < 3; i++) {
+    console.log(sortedStocks[i], stocks[sortedStocks[i]]);
+  }
 }
 
 async function start() {
   await getDailyData();
-  calculateAvg();  
+  calculateAvg();
 }
 
 start();
