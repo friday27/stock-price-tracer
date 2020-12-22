6cff20b66b1d3f4e3ad1708e206e43402835e851
friday27 <yiyinghahalin@gmail.com>
2020-12-22 22:06:11 +0000

    add linebot

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 "use strict";
 
 const express = require("express");
+const linebot = require('linebot');
+const BodyParser = require('body-parser');
 const fetchData = require("./app");
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-  console.log("Received request");
-  return res.send(fetchData());
+const bot = linebot({
+  channelId: process.env.LINE_CHANNEL_ID,
+  channelSecret: process.env.LIEN_CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+});
+
+const linebotParser = bot.parser();
+const app = express();
+
+app.post('/linewebhook', linebotParser);
+
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(BodyParser.json());
+
+app.post('/broadcast', (req, res) => {
+  bot.broadcast(req.body.message).then(() => {
+    res.send('broadcast ok');
+  }).catch(function (error) {
+    res.send('broadcast fail');
+  });
 });
 
 app.listen(port, () => {
